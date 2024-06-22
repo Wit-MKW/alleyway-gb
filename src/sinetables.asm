c8c858700a456e8d313f87a6456427858e500266
Wit <58151178+Wit-MKW@users.noreply.github.com>
2024-06-22 13:38:05 -0400

    Force higher precision when calculating sine tables
    this removes the need for the special cases i added six minutes ago.

--- src/sinetables.asm
+++ src/sinetables.asm
@@ -2,21 +2,11 @@ include "common.inc"
 setcharmap DMG
 
 macro sine_table_entry
-if (\1) == 3 && (\2) == 15
-	be $00BE
-elif (\1) == 1 && (\2) == 18
-	be $0049
-else
+	pusho
+	opt Q20
 	be ROUND(SIN(DIV((\1) * 1.0, 72.0)) * ((\2) + 8) * 32) / 1.0
-endc
-
-if (\1) == 15 && (\2) == 15
-	be $00BE
-elif (\1) == 17 && (\2) == 18
-	be $0049
-else
 	be ROUND(COS(DIV((\1) * 1.0, 72.0)) * ((\2) + 8) * 32) / 1.0
-endc
+	popo
 endm
 
 SECTION FRAGMENT "Main code", ROM0
